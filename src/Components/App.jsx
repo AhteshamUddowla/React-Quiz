e97a41182b1edcbafffff31ec98644296004bc46
AhteshamUddowla <ahteshamiiuc@gmail.com>
2023-09-04 15:40:27 +0600

    Routing

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,20 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../Styles/App.css";
 import Layout from "./Layout";
-// import Home from "./pages/Home";
-// import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
-// import Quiz from "./pages/Quiz";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <Layout>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      {/* <Quiz /> */}
-      <Result />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,14 +1,31 @@
+import { Link } from "react-router-dom";
 import classes from "../Styles/Videos.module.css";
 import Video from "./Video";
 
 function Videos() {
   return (
     <div className={classes.videos}>
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
+      <Link to="/quiz">
+        <Video />
+      </Link>
+      <Link to="/quiz">
+        <Video />
+      </Link>
+      <Link to="/quiz">
+        <Video />
+      </Link>
+      <Link to="/quiz">
+        <Video />
+      </Link>
+      <Link to="/quiz">
+        <Video />
+      </Link>
+      <Link to="/quiz">
+        <Video />
+      </Link>
+      <Link to="/quiz">
+        <Video />
+      </Link>
     </div>
   );
 }
